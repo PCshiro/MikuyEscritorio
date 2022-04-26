539d48c07daaac9686ae82f139cbdc85c9806e6d
yosepgx <46633004+yosepgx@users.noreply.github.com>
2022-04-26 16:42:16 -0500

    prueba

---
+++ ProyectosJava/LP2SoftNegocioModel/src/pe/edu/pucp/lp2soft/negocio/model/Promocion.java
@@ -0,0 +1,25 @@
+package pe.edu.pucp.lp2soft.negocio.model;
+
+import java.util.ArrayList;
+
+class Promocion extends ItemVendible{
+	private int id_promocion;
+    private ArrayList<LineaPromocion> lista_de_Comidas;//Incluye
+
+
+    // SETTERS Y GETTERS ---------------------------------------------------
+	public int getId_promocion(){
+		return id_promocion;
+	}
+	public void setId_promocion(int id_promocion){
+		this.id_promocion = id_promocion;
+	}
+
+    public ArrayList<LineaPromocion> getLista_de_Comidas() {
+		  return lista_de_Comidas;
+	}
+  	public void setLista_de_Comidas(ArrayList<LineaPromocion> lista_de_Comidas) {
+		this.lista_de_Comidas = lista_de_Comidas;
+
+	}
+}
